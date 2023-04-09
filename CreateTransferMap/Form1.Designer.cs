aa9747b11c2f83cd19b407cc610bb3859aa60a8c
JiwakabaGitGit <jiwakaba.blog@gmail.com>
2023-04-09 23:50:18 +0900

    プロジェクト ファイルを追加します。

---
+++ CreateTransferMap/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace CreateTransferMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textTextBoxWaferID = new System.Windows.Forms.TextBox();
+            this.WaferID = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown_X = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown_Y = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown_Stack = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown_SbsX = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown_SbsY = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown_SubstNumber = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numericUpDown_remainChip = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_X)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Y)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Stack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_SbsX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_SbsY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_SubstNumber)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_remainChip)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textTextBoxWaferID
+            // 
+            this.textTextBoxWaferID.Location = new System.Drawing.Point(129, 43);
+            this.textTextBoxWaferID.MaxLength = 32;
+            this.textTextBoxWaferID.Name = "textTextBoxWaferID";
+            this.textTextBoxWaferID.Size = new System.Drawing.Size(243, 23);
+            this.textTextBoxWaferID.TabIndex = 0;
+            this.textTextBoxWaferID.Text = "WaferID_0001";
+            // 
+            // WaferID
+            // 
+            this.WaferID.AutoSize = true;
+            this.WaferID.Location = new System.Drawing.Point(63, 46);
+            this.WaferID.Name = "WaferID";
+            this.WaferID.Size = new System.Drawing.Size(55, 15);
+            this.WaferID.TabIndex = 1;
+            this.WaferID.Text = "WaferID :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "基板配列数X :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "基板配列数Y :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "スタック段数 :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(74, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "SBS_X :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(74, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "SBS_Y :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(69, 252);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "基板数 :";
+            // 
+            // numericUpDown_X
+            // 
+            this.numericUpDown_X.Location = new System.Drawing.Point(130, 87);
+            this.numericUpDown_X.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_X.Name = "numericUpDown_X";
+            this.numericUpDown_X.Size = new System.Drawing.Size(77, 23);
+            this.numericUpDown_X.TabIndex = 8;
+            this.numericUpDown_X.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown_Y
+            // 
+            this.numericUpDown_Y.Location = new System.Drawing.Point(129, 123);
+            this.numericUpDown_Y.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_Y.Name = "numericUpDown_Y";
+            this.numericUpDown_Y.Size = new System.Drawing.Size(77, 23);
+            this.numericUpDown_Y.TabIndex = 9;
+            this.numericUpDown_Y.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown_Stack
+            // 
+            this.numericUpDown_Stack.Location = new System.Drawing.Point(130, 157);
+            this.numericUpDown_Stack.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown_Stack.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_Stack.Name = "numericUpDown_Stack";
+            this.numericUpDown_Stack.Size = new System.Drawing.Size(77, 23);
+            this.numericUpDown_Stack.TabIndex = 10;
+            this.numericUpDown_Stack.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown_SbsX
+            // 
+            this.numericUpDown_SbsX.Location = new System.Drawing.Point(130, 187);
+            this.numericUpDown_SbsX.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown_SbsX.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_SbsX.Name = "numericUpDown_SbsX";
+            this.numericUpDown_SbsX.Size = new System.Drawing.Size(77, 23);
+            this.numericUpDown_SbsX.TabIndex = 11;
+            this.numericUpDown_SbsX.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown_SbsY
+            // 
+            this.numericUpDown_SbsY.Location = new System.Drawing.Point(129, 217);
+            this.numericUpDown_SbsY.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown_SbsY.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_SbsY.Name = "numericUpDown_SbsY";
+            this.numericUpDown_SbsY.Size = new System.Drawing.Size(77, 23);
+            this.numericUpDown_SbsY.TabIndex = 12;
+            this.numericUpDown_SbsY.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown_SubstNumber
+            // 
+            this.numericUpDown_SubstNumber.Location = new System.Drawing.Point(129, 250);
+            this.numericUpDown_SubstNumber.Maximum = new decimal(new int[] {
+            2000000,
+            0,
+            0,
+            0});
+            this.numericUpDown_SubstNumber.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_SubstNumber.Name = "numericUpDown_SubstNumber";
+            this.numericUpDown_SubstNumber.Size = new System.Drawing.Size(77, 23);
+            this.numericUpDown_SubstNumber.TabIndex = 13;
+            this.numericUpDown_SubstNumber.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 289);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "最終基板チップ数 :";
+            // 
+            // numericUpDown_remainChip
+            // 
+            this.numericUpDown_remainChip.Location = new System.Drawing.Point(130, 289);
+            this.numericUpDown_remainChip.Maximum = new decimal(new int[] {
+            2000000,
+            0,
+            0,
+            0});
+            this.numericUpDown_remainChip.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_remainChip.Name = "numericUpDown_remainChip";
+            this.numericUpDown_remainChip.Size = new System.Drawing.Size(77, 23);
+            this.numericUpDown_remainChip.TabIndex = 15;
+            this.numericUpDown_remainChip.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 340);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 23);
+            this.textBox1.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 389);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown_remainChip);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.numericUpDown_SubstNumber);
+            this.Controls.Add(this.numericUpDown_SbsY);
+            this.Controls.Add(this.numericUpDown_SbsX);
+            this.Controls.Add(this.numericUpDown_Stack);
+            this.Controls.Add(this.numericUpDown_Y);
+            this.Controls.Add(this.numericUpDown_X);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.WaferID);
+            this.Controls.Add(this.textTextBoxWaferID);
+            this.Name = "Form1";
+            this.Text = "WaferID :";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_X)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Y)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Stack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_SbsX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_SbsY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_SubstNumber)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_remainChip)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textTextBoxWaferID;
+        private System.Windows.Forms.Label WaferID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown_X;
+        private System.Windows.Forms.NumericUpDown numericUpDown_Y;
+        private System.Windows.Forms.NumericUpDown numericUpDown_Stack;
+        private System.Windows.Forms.NumericUpDown numericUpDown_SbsX;
+        private System.Windows.Forms.NumericUpDown numericUpDown_SbsY;
+        private System.Windows.Forms.NumericUpDown numericUpDown_SubstNumber;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown numericUpDown_remainChip;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ CreateTransferMap/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CreateTransferMap
+{
+
+    public partial class Form1 : Form
+    {
+		private const int FRAME_LENGTH = 32;
+		private const string WAFER_ID = "wsafer_id";
+		private const string FRAME_BASE_ID = "frame_id_";
+		public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			try
+			{
+				// テキストボックスからファイル名 (ファイルパス) を取得
+				string fileName = this.textBox1.Text;
+
+				// SaveFileDialog クラスのインスタンスを生成
+				using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+				{
+					// ファイルの種類リストを設定
+					saveFileDialog.Filter = "テキストファイル (*.txt)|*.txt|すべてのファイル (*.*)|*.*";
+
+					// テキストボックスにファイル名 (ファイルパス) が設定されている場合は
+					// ファイルのディレクトリー (フォルダー) を初期表示する
+					if (fileName != string.Empty)
+					{
+						// FileInfo クラスのインスタンスを生成
+						FileInfo fileInfo = new FileInfo(fileName);
+						// ディレクトリー名 (ディレクトリーパス) を取得
+						string directoryName = fileInfo.DirectoryName;
+						// 存在する場合は InitialDirectory プロパティに設定
+						if (Directory.Exists(directoryName))
+						{
+							saveFileDialog.InitialDirectory = directoryName;
+						}
+					}
+
+					// ダイアログを表示
+					DialogResult dialogResult = saveFileDialog.ShowDialog();
+					if (dialogResult == DialogResult.Cancel)
+					{
+						// キャンセルされたので終了
+						return;
+					}
+
+					// 選択されたファイル名 (ファイルパス) をテキストボックスに設定
+					this.textBox1.Text = saveFileDialog.FileName;
+					this.WriteTransferMap( saveFileDialog.FileName );
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		private void WriteTransferMap( string fileName )
+		{
+			try
+			{
+				//ファイルをオープンする
+				using ( StreamWriter sw = new StreamWriter( fileName, false ) )
+				{
+					// WaferID
+					string waferId = WAFER_ID.PadRight( FRAME_LENGTH, ' ' );
+					sw.Write( waferId );
+					// FrameID
+					string frameId = this.textTextBoxWaferID.Text.PadRight( FRAME_LENGTH, ' ' );
+					sw.Write( frameId );
+
+					sw.Write( "xxxx" );
+					sw.Write( "xxxx" );
+					sw.Write( "xxxx" );
+
+					// チップ配列数
+					int chipArrayX = decimal.ToInt32( this.numericUpDown_X.Value ) * decimal.ToInt32(this.numericUpDown_SbsX.Value);
+					sw.Write( chipArrayX.ToString("X4") );
+					int chipArrayY = decimal.ToInt32( this.numericUpDown_Y.Value ) * decimal.ToInt32( this.numericUpDown_SbsY.Value ) * decimal.ToInt32( this.numericUpDown_Stack.Value );
+					sw.Write( chipArrayY.ToString( "X4" ) );
+
+					for(int i = 0; i < 3; i++ )
+                    {
+						for ( int y = 0; y < 4; y++ )
+						{
+							sw.Write( "xxx/" );
+						}
+					}
+
+					// 基板配列数XY
+					int boardArrayX = decimal.ToInt32( this.numericUpDown_X.Value );
+					sw.Write( boardArrayX.ToString( "X4" ) );
+					int boardArrayY = decimal.ToInt32( this.numericUpDown_Y.Value );
+					sw.Write( boardArrayY.ToString( "X4" ) );
+
+					// スタック段数
+					int stuckNumber = decimal.ToInt32( this.numericUpDown_Stack.Value );
+					sw.Write( stuckNumber.ToString( "X4" ) );
+
+					// アイランド配列数XY(SBS)
+					int islandArrayX = decimal.ToInt32( this.numericUpDown_SbsX.Value );
+					sw.Write( islandArrayX.ToString( "X4" ) );
+					int islandArrayY = decimal.ToInt32( this.numericUpDown_SbsY.Value );
+					sw.Write( islandArrayY.ToString( "X4" ) );
+
+					//	基板数
+					int substraiteNumber = decimal.ToInt32( this.numericUpDown_SubstNumber.Value );
+					sw.Write( substraiteNumber.ToString( "X4" ) );
+
+					int iSubstCounter = 1;
+					int iSequenceOrder = 0;
+					for ( iSubstCounter = 1; iSubstCounter < substraiteNumber; iSubstCounter++ )
+                    {
+						string substraiteID = FRAME_BASE_ID + iSubstCounter.ToString( "X4" );
+						sw.Write( substraiteID.PadRight( FRAME_LENGTH, ' ' ) );
+
+						// チップ数
+						int iChipNumber = chipArrayX * chipArrayY;
+						sw.Write( iChipNumber.ToString( "X4" ) );
+
+						for ( int iTxIndex = 1; iTxIndex <= chipArrayX; iTxIndex++ )
+                        {
+							for ( int i = 0; i < boardArrayY; i++ )
+                            {
+								for ( int j = 0; j < islandArrayY; j++ )
+                                {
+									for ( int iZ = 1; iZ <= stuckNumber; iZ++ )
+									{
+										int iFx = iSequenceOrder;
+										sw.Write( iFx.ToString( "X4" ) );
+										int iFy = iSubstCounter;
+										sw.Write( iFy.ToString( "X4" ) );
+
+										sw.Write( iTxIndex.ToString( "X4" ) );
+										int iTyIndex = ( islandArrayY * stuckNumber ) *  j + ( stuckNumber * i ) + iZ;
+										sw.Write( iTyIndex.ToString( "X4" ) );
+										sw.Write( iZ.ToString( "X4" ) );
+
+										iSequenceOrder++;
+									}
+								}
+                            }
+                        }
+                    }
+
+					string substraiteLastId = FRAME_BASE_ID + iSubstCounter.ToString( "X4" );
+					sw.Write( substraiteLastId.PadRight( FRAME_LENGTH, ' ' ) );
+
+					//	最終基板チップ基板数
+					int chipRemain = decimal.ToInt32( this.numericUpDown_remainChip.Value );
+					if ( chipArrayX * chipArrayY < chipRemain )
+                    {
+						chipRemain = chipArrayX * chipArrayY;
+					}
+
+					sw.Write( chipRemain.ToString( "X4" ) );
+
+					for ( int iTxIndex = 1; iTxIndex <= chipArrayX; iTxIndex++ )
+					{
+						if ( chipRemain < 1 )
+						{
+							break;
+						}
+						
+						for ( int i = 0; i < boardArrayY; i++ )
+						{
+							if ( chipRemain < 1 )
+							{
+								break;
+							}
+
+							for ( int j = 0; j < islandArrayY; j++ )
+							{
+								if ( chipRemain < 1 )
+								{
+									break;
+								}
+
+								for ( int iZ = 1; iZ <= stuckNumber; iZ++ )
+								{
+									if ( chipRemain < 1 )
+                                    {
+										break;
+                                    }
+									int iFx = iSequenceOrder;
+									sw.Write( iFx.ToString( "X4" ) );
+									int iFy = iSubstCounter;
+									sw.Write( iFy.ToString( "X4" ) );
+
+									sw.Write( iTxIndex.ToString( "X4" ) );
+									int iTyIndex = ( islandArrayY * stuckNumber ) * j + ( stuckNumber * i ) + iZ;
+									sw.Write( iTyIndex.ToString( "X4" ) );
+									sw.Write( iZ.ToString( "X4" ) );
+
+									iSequenceOrder++;
+
+									chipRemain--;
+								}
+							}
+						}
+					}
+				}
+			}
+			catch ( Exception ex )
+			{
+				MessageBox.Show( ex.Message );
+			}
+		}
+	}
+}
